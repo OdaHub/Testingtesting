25dbd2341053694c2e2fe288ea7f9d399a35b4fb
Oda Dahlen <oda.dahlen@ntnu.no>
2015-07-28 10:48:49 +0200

    Deleted FFS

--- FFS_Bordeaux/FFS_bordeaux/directory_Bordeaux/analyze/fexc.sh
+++
@@ -1,5 +0,0 @@
- gnuplot DW.gnu
- latex TISMOL.tex
- dvips -o -t a4 TISMOL.dvi
- ps2pdf TISMOL.ps
- gv TISMOL.ps & 
--- FFS_Bordeaux/FFS_bordeaux/directory_Bordeaux/tism/defaultfile.f90
+++
@@ -1,7 +0,0 @@
-!BM---------------------------------------------------
-module defaultfile
-  use stringlengths
-  character(len=LSTR), parameter::inputdefault="TISMOL.DEFAULT"
-end module defaultfile
-!EM---------------------------------------------------
-
